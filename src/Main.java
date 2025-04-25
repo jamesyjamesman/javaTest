5a36e3170f921949e1b02d408850837c6d0b85da
jamesyjamesman <jameson.price@my.uwrf.edu>
2025-04-25 17:25:44 -0500

    Adds another class!

--- src/Main.java
+++ src/Main.java
@@ -10,9 +10,18 @@ public class Main {
         boolean invertedCoolBool = boolSwitch(coolBool);
         System.out.printf("The opposite of what you just said is: " + invertedCoolBool +"!");
         System.out.println();
+        NotMain.notMain();
     }
 
     public static boolean boolSwitch(boolean coolBool) {
         return !coolBool;
+    }
+}
+
+class NotMain {
+    public static void notMain() {
+        System.out.println("If I can get this string to print I'll be happy!");
+        System.out.println("I wonder if there's any other way to get a class to run without directly calling a function inside of it?");
+        System.out.println("Classes are used for OOP or for organization, I believe.");
     }
 }
