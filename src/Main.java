72b37933f3bc3403f3c22facdc29cf63ed399700
jamesyjamesman <jameson.price@my.uwrf.edu>
2025-04-25 17:41:40 -0500

    Adds another class that contains a swtich test

--- src/Main.java
+++ src/Main.java
@@ -11,6 +11,7 @@ public class Main {
         System.out.printf("The opposite of what you just said is: " + invertedCoolBool +"!");
         System.out.println();
         NotMain.notMain(coolBool);
+        SwitchTest.mainSwitcheroo();
     }
 
     public static boolean boolSwitch(boolean coolBool) {
@@ -29,6 +30,49 @@ class NotMain {
             System.out.println("Hey, you're not a truther!");
         } else {
             System.out.println("Hey, uh, how did you get here?");
+        }
+    }
+}
+
+class SwitchTest {
+    public static void mainSwitcheroo() {
+        Scanner textScanner = new Scanner(System.in);
+        System.out.println("Pick a number from 0-9.");
+        String inputNumber = textScanner.nextLine();
+        switch (inputNumber.charAt(0)) {
+            case '0':
+                System.out.println("Entropic... I like it!");
+                break;
+            case '1':
+                System.out.println("This is what you always roll in board games, huh?");
+                break;
+            case '2':
+                System.out.println("An even individual, I see.");
+                break;
+            case '3':
+                System.out.println("That's how many hits the boss takes to beat!");
+                break;
+            case '4':
+                System.out.println("Four... Unlucky, to some people. Don't walk under any ladders!");
+                break;
+            case '5':
+                System.out.println("The midpoint! Well, kind of.");
+                break;
+            case '6':
+                System.out.println("You better not roll this too much, or you'll never roll it again.");
+                break;
+            case '7':
+                System.out.println("Lucky!");
+                break;
+            case '8':
+                System.out.println("The most programmatic number there is from 0-9!");
+                break;
+            case '9':
+                System.out.println("You just picked the first number you saw, didn't you?!");
+                break;
+            default:
+                System.out.println("I'm just realizing that it would have been easier to assign each string to a variable, then I'd only have to write one print statement!");
+                break;
         }
     }
 }
