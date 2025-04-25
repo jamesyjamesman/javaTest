d58e349ee06190f39afca39890b0f40dff56b7c8
jamesyjamesman <jameson.price@my.uwrf.edu>
2025-04-25 17:29:21 -0500

    Adds a conditional!

--- src/Main.java
+++ src/Main.java
@@ -10,7 +10,7 @@ public class Main {
         boolean invertedCoolBool = boolSwitch(coolBool);
         System.out.printf("The opposite of what you just said is: " + invertedCoolBool +"!");
         System.out.println();
-        NotMain.notMain();
+        NotMain.notMain(coolBool);
     }
 
     public static boolean boolSwitch(boolean coolBool) {
@@ -19,9 +19,16 @@ public class Main {
 }
 
 class NotMain {
-    public static void notMain() {
+    public static void notMain(boolean coolBool) {
         System.out.println("If I can get this string to print I'll be happy!");
         System.out.println("I wonder if there's any other way to get a class to run without directly calling a function inside of it?");
         System.out.println("Classes are used for OOP or for organization, I believe.");
+        if (coolBool) {
+            System.out.println("Hey, you're a truther!");
+        } else if (!coolBool) {
+            System.out.println("Hey, you're not a truther!");
+        } else {
+            System.out.println("Hey, uh, how did you get here?");
+        }
     }
 }
