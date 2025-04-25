986c141942d4438d102d5fb190a5c9a123523f2c
jamesyjamesman <jameson.price@my.uwrf.edu>
2025-04-25 17:11:33 -0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hi there! Welcome to my doodad here.");
+        Scanner textScanner = new Scanner(System.in);
+        System.out.println("State a value that is True or False!");
+        String coolBoolStr = textScanner.nextLine();
+        boolean coolBool = Boolean.parseBoolean(coolBoolStr);
+        boolean invertedCoolBool = boolSwitch(coolBool);
+        System.out.printf("The opposite of what you just said is: " + invertedCoolBool +"!");
+        System.out.println();
+    }
+
+    public static boolean boolSwitch(boolean coolBool) {
+        return !coolBool;
+    }
+}
